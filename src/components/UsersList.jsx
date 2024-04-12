e45c52620f0f9fabc4402a0d991205c8246e41e2
Saadat Hassan <saadathassan55@yahoo.com>
2024-04-12 00:05:41 -0400

    add user fetch thunk

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
 
 export const UsersList = () => {
-  return <div>UsersList</div>;
+  const dispatch = useDispatch();
+
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]); // here we can leave dependency array empty as well
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data</div>;
+  }
+
+  return <div>{data.length}</div>;
 };
